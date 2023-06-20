e5380c1ad33d13b90dd664a3dda47f8440a13945
Jack-00000 <129925697+Jack-00000@users.noreply.github.com>
2023-06-20 06:22:16 +0000

    [update]マイグレーションファイル

--- db/migrate/20230619123620_devise_create_users.rb
+++ db/migrate/20230619123620_devise_create_users.rb
@@ -32,6 +32,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :name, null: false
+      t.text :introduction
+      t.integer :status, null: false, default: 0
 
       t.timestamps null: false
     end
--- db/migrate/20230619142543_create_posts.rb
+++ db/migrate/20230619142543_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      t.integer :user_id, null: false
+      t.text :goal, null: false
+      t.text :active, null: false
+     
       t.timestamps
     end
   end
