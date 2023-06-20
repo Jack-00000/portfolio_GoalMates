c6a689a8810ffeb3bd08c3ad83f18948e3e2c355
Jack-00000 <129925697+Jack-00000@users.noreply.github.com>
2023-06-20 08:01:08 +0000

    [Update]ゲストログイン

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -18,6 +18,12 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user), notice: 'guestuserでログインしました。'
+  end
+  
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,35 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
   validates :status, presence: true
-         
+  
   has_one_attached :profile_image
   
+  enum status: { released: 0, nonreleased: 1, withdraw: 2 }
+
+  #ユーザーステータスが”退会”以外のユーザーをユーザー一覧で表示させるためのscope
+  scope :active_user, -> { where(status: 0).or(where(status: 1)) }
+
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def active_for_authentication?
+    super && (self.status == 'released')
+  end
+
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
+
 end
