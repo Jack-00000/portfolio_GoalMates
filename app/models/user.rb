1658876f6ebedae38ab5ccc3581c27b18f267875
Jack-00000 <129925697+Jack-00000@users.noreply.github.com>
2023-06-20 05:45:43 +0000

    [Update]モデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+  validates :status, presence: true
+         
+  has_one_attached :profile_image
+  
+  has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
