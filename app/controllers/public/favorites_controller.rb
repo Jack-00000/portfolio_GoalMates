ed41e6f34eae3fdd5bb38baa528cba411d37db8d
Jack-00000 <129925697+Jack-00000@users.noreply.github.com>
2023-06-27 23:02:43 +0000

    [fix]お気に入り非同期

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -5,13 +5,11 @@ class Public::FavoritesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.favorites.find_or_create_by(post_id: @post.id)
-    render :favorite
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: @post.id)
-    favorite.destroy if favorite
-    render :favorite
+    favorite.destroy
   end
 end
