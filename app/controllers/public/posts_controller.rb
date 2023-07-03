f9e1a9b7969f0851c4ec538ccd313e8cfcb50e89
Jack-00000 <129925697+Jack-00000@users.noreply.github.com>
2023-07-03 10:05:31 +0000

    [Add]API

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -13,6 +13,7 @@ class Public::PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
+    @post.score = Language.get_data(post_params[:action])
     if @post.save
       flash[:notice] = "投稿に成功しました"
       redirect_to post_path(@post.id)
